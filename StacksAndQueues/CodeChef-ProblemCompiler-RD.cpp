4674d72656745f4fd5a5416f459effdcf72d772e
Richi Dubey <herestobeingawesome@gmail.com>
2018-11-23 01:18:13 +0530

    Changed Pradyuman's code and added my COMPILER's solution

---
+++ StacksAndQueues/CodeChef-ProblemCompiler-RD.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#include<string>
+ #include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,t,curr,here,maxcurr;
+	
+	cin>>t;
+	
+	vector <char> inp;
+	
+	while(t--)
+	{
+		maxcurr=0;
+		curr=0;
+		here=0;
+		
+		
+		string in;
+		
+		cin>>in;
+		
+		i=0;
+		while(i<in.size()) 
+		{
+		
+	
+			if(in[i]=='>')
+			{
+				if(inp.size()!=0)
+				{
+					inp.pop_back();
+					curr+=2;
+				}
+				else break;
+
+			}
+			else
+			{	inp.push_back(in[i]);
+			}
+			
+			if(curr>maxcurr&&inp.size()==0)
+				maxcurr=curr;
+			
+			i++;
+			
+			
+		}
+	
+		cout<<maxcurr<<endl;
+		inp.clear();				
+	}
+	
+	return 0;
+}
+
