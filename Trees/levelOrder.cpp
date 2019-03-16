4f0861ae52aada824f76f25f928d76ca854b02c0
Richi <herestobeingawesome@gmail.com>
2019-03-16 23:54:45 +0530

    LevelOrder

---
+++ Trees/levelOrder.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>	
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+    queue <Node *> put;
+
+    void levelOrder(Node * root) {
+
+        if(root==NULL)
+        return;
+
+        put.push(root->left);
+        put.push(root->right);
+
+        cout<<put.front()->data<<" ";
+        put.pop();
+
+        levelOrder(put.front());
+
+    }
+
+}; //End of Solution
